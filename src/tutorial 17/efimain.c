2c4cf2cb492fbcbc55b52a9693318372246966c9
ThatOSDev <starlordgo@protonmail.com>
2021-06-26 15:49:26 -0400

    Create and Write to a file

---
+++ src/tutorial 17/efimain.c
@@ -0,0 +1,79 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#include "efi.h"
+#include "ErrorCodes.h"
+#include "clibs.h"
+#include "efilibs.h"
+
+EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *ST)
+{
+    ImageHandle = IH;
+    SystemTable = ST;
+    
+    ResetScreen();
+    
+    SetColor(EFI_WHITE);
+    SetTextPosition(3, 2);
+    Print(L"Welcome to the UEFI From Scratch Tutorials");
+    
+    SetColor(EFI_GREEN);
+    SetTextPosition(8, 4);
+    Print(L"Hit Any Key to write to a file\r\n");
+
+    HitAnyKey();
+
+    ClearScreen();
+	
+    // Writing to a file
+	InitializeFILESYSTEM();
+	
+	char* testbuffer = "Hello World";
+	
+	EFI_FILE_PROTOCOL* writefilehandle = createFile(L"test.txt");
+	if(writefilehandle != NULL)
+	{
+		UINT64 fileSize = 11; // 0x0000000B;
+		
+		SetColor(EFI_BROWN);
+		Print(L"Writing to File ... ");
+		EFI_STATUS Status = writefilehandle->Write(writefilehandle, &fileSize, testbuffer);
+		SetColor(EFI_CYAN);
+		Print(CheckStandardEFIError(Status));
+
+		closeFile(writefilehandle);
+	}
+
+    SetColor(EFI_GREEN);
+    SetTextPosition(8, 10);
+    Print(L"Hit q to quit | Hit r to reboot");
+
+    ResetKeyboard();
+
+    while(1)
+    {
+		Delay1();
+		
+		EFI_STATUS Status = CheckKey();
+		if(Status == EFI_SUCCESS)
+		{
+			if(GetKey('q') == 1)
+			{
+				SHUTDOWN();
+				break;
+			}
+			if(GetKey('r') == 1)
+			{
+				COLD_REBOOT();
+				break;
+			}
+		}
+
+	}
+
+    // We should not make it to this point.
+    COLD_REBOOT();
+
+    // We should not make it to this point.
+    return EFI_SUCCESS;
+}
