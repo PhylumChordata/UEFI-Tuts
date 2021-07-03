36f4022e0aec1cb9461c8c1d1da8d9db21d8e32f
ThatOSDev <starlordgo@protonmail.com>
2021-07-03 10:33:25 -0400

    Fixed makefile and bat file -- removed freestanding from EFI

--- src/tutorial 23/compileEFI.bat
+++ src/tutorial 23/compileEFI.bat
@@ -1,4 +1,4 @@
-gcc -Wall -Werror -m64 -mabi=ms -ffreestanding -I ThatCLib -c efimain.c -o efimain.o
+gcc -Wall -Werror -m64 -mabi=ms -I ThatCLib -c efimain.c -o efimain.o
 gcc -Wall -Werror -m64 -mabi=ms efimain.o -nostdlib -shared -Wl,-dll -Wl,--subsystem,10 -e efi_main -o BOOTX64.EFI
 
 gcc -Wall -Werror -m64 -mabi=ms -ffreestanding -Wl,--oformat=binary -e main -c loader/loader.c -o loader/loader.bin
