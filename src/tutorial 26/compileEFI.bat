0203199fcd160f1f3a755236931b887be0bdf03b
ThatOSDev <starlordgo@protonmail.com>
2021-07-17 12:10:29 -0400

    Removed Optimization

--- src/tutorial 26/compileEFI.bat
+++ src/tutorial 26/compileEFI.bat
@@ -1,7 +1,7 @@
 gcc -Wall -Werror -m64 -mabi=ms -I ThatCLib -c efimain.c -o efimain.o
 gcc -Wall -Werror -m64 -mabi=ms efimain.o -nostdlib -shared -Wl,-dll -Wl,--subsystem,10 -e efi_main -o BOOTX64.EFI
 
-gcc -Wall -Werror -m64 -mabi=ms -ffreestanding -Wl,--oformat=binary -e main -c loader/loader.c -o loader/loader.bin
+gcc -Wall -Werror -O0 -m64 -mabi=ms -ffreestanding -Wl,--oformat=binary -e main -c loader/loader.c -o loader/loader.bin
 
 @echo off
 del /Q *.o
