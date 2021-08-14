2847adeaa37d34c8ad5e0beaacedecbb30504067
ThatOSDev <starlordgo@protonmail.com>
2021-08-14 07:23:01 -0400

    Initialize GDT before Jumping to Loader

--- src/tutorial 30/compileEFI.bat
+++ src/tutorial 30/compileEFI.bat
@@ -1,9 +1,3 @@
-@echo off
-rem cd gdt
-rem fasm gdt.asm
-rem cd ..
-@echo on
-
 gcc -Wall -Werror -m64 -Wno-unused-variable -mabi=ms -I ThatCLib -c efimain.c -o efimain.o
 gcc -Wall -Werror -m64 -mabi=ms efimain.o -nostdlib -shared -Wl,-dll -Wl,--subsystem,10 -e efi_main -o BOOTX64.EFI
 
--- src/tutorial 30/mountdrive.bat
+++ src/tutorial 30/mountdrive.bat
@@ -2,10 +2,6 @@ OSFMount -a -t file -f ..\..\drive\drive.hdd -s 40M -o rw -m F:
 
 copy /y BOOTX64.EFI "F:\EFI\Boot\"
 
-@echo off
-rem move /y gdt\gdt.bin "F:\ThatOS64\"
-@echo on
-
 move /y loader\loader.bin "F:\ThatOS64\"
 
 timeout 3
